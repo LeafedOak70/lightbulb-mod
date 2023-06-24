c792f852a0d7933383f77e227126ea484c736e4a
Dulguun Enkhtsog <dulguun0614@gmail.com>
2023-06-24 13:25:43 -0400

    Initial commit

---
+++ src/main/java/net/leaf/lightbulb/LightBulb.java
@@ -0,0 +1,18 @@
+package net.leaf.lightbulb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LightBulb implements ModInitializer {
+
+	public static final String MOD_ID = "lightbulb";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/leaf/lightbulb/LightBulbClient.java
@@ -0,0 +1,11 @@
+package net.leaf.lightbulb;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightBulbClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
